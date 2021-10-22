e7798b276bbb020075d262426abc54af995f7431
Fillipi Pereira <pfillipi@gmx.de>
2021-10-22 04:17:33 +0200

    List

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect } from "react";
+import { Can } from "../components/Can";
 import { AuthContext } from "../contexts/AuthContext";
 import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
@@ -15,7 +16,8 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      <Can>
+
+      <Can permissions={["metrics.list"]}>
         <div>Beregninger</div>
       </Can>
     </>
