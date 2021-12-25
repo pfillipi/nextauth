6fa3199a9e11895c57b8a07b16df34bab865932d
Fillipi Pereira <pfillipi@gmx.de>
2021-12-25 03:59:26 +0100

    Some roles

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -9,7 +9,7 @@ export default function Dashboard() {
   const { user, isAuthenticated } = useContext(AuthContext);
 
   const userCanSeeMetrics = useCan({
-    roles: ["editor"],
+    roles: ["administrator", "editor"],
   });
 
   useEffect(() => {
