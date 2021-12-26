eb59ef9f2862b7c4753cc7d58761327d1c5be31f
Fillipi Pereira <pfillipi@gmx.de>
2021-12-26 19:39:03 +0100

    Decode

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect } from "react";
-import { Can } from "../components/Can";
+
 import { AuthContext } from "../contexts/AuthContext";
-import { useCan } from "../hooks/useCan";
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
+import { useCan } from "../hooks/useCan";
+import { Can } from "../components/Can";
 
 export default function Dashboard() {
   const { user, isAuthenticated } = useContext(AuthContext);
